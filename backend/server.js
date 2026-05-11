dfac1889fb4f6f862a42748614dce06b208fc0ba
Luiz Alves <lulamattos@gmail.com>
2026-05-11 16:22:13 -0300

    Initial commit: Setup React (Vite) and Node.js (Express)

---
+++ backend/server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('API de Integração Rodando!'));
+app.listen(3000, () => console.log('Servidor na porta 3000'));
